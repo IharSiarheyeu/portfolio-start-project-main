1d0ce9c1f4a285520aae2c38df709ecbfdc5ffc4
Ihar Siarheyeu <igor4093@gmail.com>
2024-07-22 16:26:19 +0300

    Added header styles

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
+import {theme} from "../../styles/Theme";
+
+const headerItems = ["Home", "Skills", "Works", "Testimony", "Contact"];
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <HeaderMenu menuItems={headerItems}/>
+                    </FlexWrapper>
+            </Container>
+        </StyledHeader>
+
+)
+    ;
+};
+
+const StyledHeader = styled.header`
+    background-color: ${theme.colors.primaryBg};
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
+`
+
+
+export default Header;
